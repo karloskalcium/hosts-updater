3fe535a3dddf03fde26cc436b8bbe4003caae91b
Karl Brown <karl@cs.stanford.edu>
2023-01-09 20:16:30 -0500

    Initial commit of hosts file updater wrapper script

---
+++ update-hosts.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Build and replace new hosts file with whitelist in local dir
+# from https://github.com/StevenBlack/hosts
+set -eou pipefail
+
+docker run --pull always -it -v /etc/hosts:/etc/hosts \
+-v "/Users/kbrown/dev/util/host-updater/whitelist:/hosts/whitelist" \
+-v "/Users/kbrown/dev/util/host-updater/hosts_output:/hosts/hosts_output" \
+ghcr.io/stevenblack/hosts:latest updateHostsFile.py --auto \
+--output hosts_output --extensions fakenews --whitelist whitelist
+
+echo "Done generating hosts file, now moving to /etc/hosts"
+sudo cp -f /private/etc/hosts "/private/etc/hosts.bak.$(date +%Y%m%d_%H%M%S)"
+sudo mv -f hosts_output/hosts /private/etc/hosts
+
+# flush dns cache
+echo "Flushing cache"
+sudo dscacheutil -flushcache
+sudo killall -HUP mDNSResponder
+
+echo "Cache flushed"
+
+echo "Opening chrome so you can clear cache in chrome"
+open -a "Google Chrome.app" "chrome://net-internals/#dns"
+echo "All done!"
